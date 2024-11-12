824c4f5be70c7518d5fcc4e290612fb378fef19b
RodrigoMoreno2001 <rodrigoinsti06@gmail.com>
2024-11-12 16:35:08 +0100

    primer commit

---
+++ src/main/java/com/mycompany/tiendahilos/TiendaHilos.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.tiendahilos;
+
+/**
+ *
+ * @author alumno
+ */
+public class TiendaHilos {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
