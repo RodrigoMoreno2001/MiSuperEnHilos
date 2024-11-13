59b1fb3555f81da781c4992e688171c16561bb1f
RodrigoMoreno2001 <rodrigoinsti06@gmail.com>
2024-11-13 13:41:32 +0100

    problemas con la concurrencia

--- src/main/java/com/mycompany/tiendahilos/Caja.java
+++ src/main/java/com/mycompany/tiendahilos/Caja.java
@@ -1,15 +1,23 @@
 
 package com.mycompany.tiendahilos;
 
+import java.util.concurrent.Semaphore;
+
 
 public class Caja {
    private String nombreCaja;
+   private Semaphore sem;
    
-   public Caja(String nombreCaja){
+   public Caja(String nombreCaja, Semaphore sem){
        this.nombreCaja=nombreCaja;
+       this.sem=sem;
    }
     
    public void pasarPorCaja(Cliente cliente){
+       
+       System.out.println("Bienvenido "+ cliente.getNombreCliente()+ " a la caja "+nombreCaja);
+       
+       for(String aux:cliente.getProductos())System.out.println("Pasando por caja producto: "+aux);
        
    }
    
--- src/main/java/com/mycompany/tiendahilos/Cliente.java
+++ src/main/java/com/mycompany/tiendahilos/Cliente.java
@@ -1,15 +1,17 @@
 
 package com.mycompany.tiendahilos;
 
-public class Cliente {
+public class Cliente implements Runnable{
     private String nombreCliente;
     private final String[] posiblesProductos = { "Croquetas para perros", "Snacks de carne deshidratada", "Alimento húmedo enlatado", "Comida balanceada para cachorros", "Galletas para perro sabor pollo", "Alimento hipoalergénico", "Suplemento nutricional para perros", "Bocadillos naturales de pollo", "Comida vegana para perros", "Premios dentales", "Hueso relleno de carne", "Snacks de salmón", "Comida para perros mayores", "Premios sabor tocino", "Croquetas sin cereales", "Galletas de avena y miel", "Puré de verduras para perros", "Bocadillos de pavo", "Alimento orgánico para perros", "Snacks de hígado de pollo" };
     private String[] productos;
+    private Caja[] cajas;
             
-    public Cliente(String nombreCliente,int productos){
+    public Cliente(String nombreCliente,int productos,Caja[] cajas){
         this.nombreCliente=nombreCliente;
         this.productos=new String[productos];       
-        for(int i=0;i<productos;i++) this.productos[i]=posiblesProductos[(int) (Math.random()*posiblesProductos.length)];   
+        for(int i=0;i<productos;i++) this.productos[i]=posiblesProductos[(int) (Math.random()*posiblesProductos.length)];
+        this.cajas=cajas;
     }
 
     public String getNombreCliente() {
@@ -26,6 +28,13 @@ public class Cliente {
 
     public void setProductos(String[] productos) {
         this.productos = productos;
+    }
+
+    @Override
+    public void run() {
+        for(Caja aux:this.cajas){
+            aux.pasarPorCaja(this);
+        }
     }
     
     
